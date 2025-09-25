7cdf5238cc457eeb662b7764e03b11f98c18765c
John Dorman <namrods15@gmail.com>
2025-09-25 22:21:47 +0200

    Start 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, elementToRemove) {
+    updatedArr = []   
+
+    arr.forEach(element => {
+        if (element !== elementToRemove) {
+          updatedArr.push(element)
+        }
+    });
+
+    return updatedArr
 };
 
 // Do not edit below this line
