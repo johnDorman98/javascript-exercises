d9774ac373e5291a8020b72799a9680cf4788d1a
John Dorman <namrods15@gmail.com>
2025-09-25 22:03:14 +0200

    Complete 06_repeatString to pass tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repeatCount) {
+    let repeatedString = ""
 
+    if (repeatCount < 0) {
+      return "ERROR"
+    }
+
+    if (!stringToRepeat) {
+      return repeatedString;
+    }
+    
+    for (let i = 0; i < repeatCount; i++) {
+      repeatedString = repeatedString + stringToRepeat
+    }
+
+    return repeatedString
 };
+
+repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
