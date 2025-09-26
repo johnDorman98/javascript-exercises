4be994ea70cd1195f8b32e3283dfa025b6ad32c2
John Dorman <namrods15@gmail.com>
2025-09-26 13:51:52 +0200

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // Only allow integers
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR"
+  }
+  
+  // Ensure positive number is given
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR"
+  }
+  
+  // Swap num1 and num2 when num1 > num2
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  
+  let sum = 0;
 
+  // Add each number in the range of num1 and num2 to sum.
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
