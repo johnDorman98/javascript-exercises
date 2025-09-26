00382d31b1430a6e491674b8f395e80cd9c41690
John Dorman <namrods15@gmail.com>
2025-09-26 22:08:54 +0200

    Complete 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // let isLeapYear = false
 
+  // Nested conditional approach
+  // if (year % 4 === 0) {
+  //     if (year % 100 === 0 && year % 400 !== 0) {
+  //         return isLeapYear
+  //     }
+  //     isLeapYear = true
+  // }
+
+  // return isLeapYear
+
+  // Single conditional approach
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
