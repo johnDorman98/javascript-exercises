cc580e2d0032b7055741da5be6066fac9a140833
John Dorman <namrods15@gmail.com>
2025-10-18 22:49:51 +0200

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Convert to lowercase and remove special characters.
+  string = string.replace(/[\W_]+/g, "").toLowerCase();
 
+  // Store reversed string
+  let reversedString = string.split("").reverse().join("");
+
+  return string === reversedString;
 };
 
 // Do not edit below this line
