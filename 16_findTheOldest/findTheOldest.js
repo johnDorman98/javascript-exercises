f4802cb6c743ca848909a9154e3d9367b76221f8
John Dorman <namrods15@gmail.com>
2025-10-19 12:16:35 +0200

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((previousPerson, currentPerson) => {
+    const currentYear = new Date().getFullYear();
 
+    if (!Object.hasOwn(previousPerson, "yearOfDeath")) {
+      previousPerson.age = currentYear - previousPerson.yearOfBirth;
+    }
+
+    if (!Object.hasOwn(currentPerson, "yearOfDeath")) {
+      currentPerson.age = currentYear - currentPerson.yearOfBirth;
+    }
+
+    if (!Object.hasOwn(previousPerson, "age")) {
+      previousPerson.age =
+        previousPerson.yearOfDeath - previousPerson.yearOfBirth;
+    }
+
+    if (!Object.hasOwn(currentPerson, "age")) {
+      currentPerson.age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    }
+
+    if (currentPerson.age > previousPerson.age) {
+      return currentPerson;
+    }
+
+    return previousPerson;
+  }, people[0]);
 };
 
 // Do not edit below this line
