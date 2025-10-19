08c144326faa935b4b91600778b050c968de91fb
John Dorman <namrods15@gmail.com>
2025-10-19 11:47:10 +0200

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,39 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    // Iterative approach using an array to build the fibonachi numbers.
+    // if (!Number.isInteger(number)) {
+    //     number = Number(number)
+    // }
 
+    // if (number == 0) {
+    //     return 0
+    // }
+
+    // if (number < 0) {
+    //     return "OOPS"
+    // }
+    
+    // const fibonacciNumbers = [0, 1]
+
+    // for (let i = 2; i <= number; i++) {        
+    //     fibonacciNumbers[i] = fibonacciNumbers[i-1] + fibonacciNumbers[i-2]        
+    // }
+
+    // return fibonacciNumbers[number]
+
+    // Recursive approach.
+    if (number == 0) {
+        return 0
+    }
+
+    if (number == 1) {
+        return 1
+    }
+
+    if (number < 0) {
+        return "OOPS"
+    }
+
+    return fibonacci(number-1) + fibonacci(number-2)
 };
 
 // Do not edit below this line
